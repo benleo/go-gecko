c3b6ec872bc83724d2fd6c86e88e9cb97eabb2e3
yoojia.chen <yoojiachen@gmail.com>
2019-01-08 09:25:04 +0800

    Add comment

--- interceptor.go
+++ interceptor.go
@@ -4,9 +4,12 @@ package gecko
 // Author: 陈哈哈 chenyongjia@parkingwang.com, yoojiachen@gmail.com
 //
 
+// 事件拦截器。在Gecko系统中，通过Trigger触发事件后，由 Interceptor 处理拦截。
+// 负责对触发器发起的事件进行拦截处理，不符合规则的事件将被中断，丢弃。
 type Interceptor interface {
 	InitialAware
 
+	// Interceptor可设置优先级
 	GetPriority() int
 	SetPriority(p int)
 
