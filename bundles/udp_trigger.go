f281fb81880f4e1d3b0ab4dd5d3ffe79ff60b516
yoojia <yoojiachen@gmail.com>
2019-01-09 09:13:40 +0800

    更新Encoder/Decoder

--- bundles/udp_trigger.go
+++ bundles/udp_trigger.go
@@ -5,7 +5,6 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/yoojia/go-gecko"
-	"github.com/yoojia/go-gecko/x"
 	"net"
 	"time"
 )
@@ -20,36 +19,21 @@ type UdpServerTrigger struct {
 	udpAddr *net.UDPAddr
 	udpConn *net.UDPConn
 	// 数据序列化与反序列化
-	decoder func(bytes []byte) map[string]interface{}
-	encoder func(json map[string]interface{}) []byte
+	decoder gecko.Decoder
+	encoder gecko.Encoder
 	topic   string
 	// UDP发送/接收设置
 	sendTimeout time.Duration
 	recvTimeout time.Duration
-	buffSize    int64
+	bufferSize  int64
 	//
 	shutdownCompleted chan struct{}
 }
 
 func (ust *UdpServerTrigger) OnInit(args map[string]interface{}, scoped gecko.GeckoScoped) {
 	ust.shutdownCompleted = make(chan struct{}, 1)
-	ust.decoder = func(bytes []byte) map[string]interface{} {
-		json := make(map[string]interface{})
-		if err := x.UnmarshalJSON(bytes, json); nil != err {
-			ust.withTag(log.Error).Err(err).Msg("解析JSON数据发生错误")
-			return nil
-		} else {
-			return json
-		}
-	}
-	ust.encoder = func(json map[string]interface{}) []byte {
-		if bytes, err := x.MarshalJSON(json); nil != err {
-			ust.withTag(log.Error).Err(err).Msg("序列化JSON数据发生错误")
-			return nil
-		} else {
-			return bytes
-		}
-	}
+	ust.decoder = gecko.JSONDefaultDecoder
+	ust.encoder = gecko.JSONDefaultEncoder
 	config := conf.MapToMap(args)
 
 	address := config.MustString("listenAddress")
@@ -58,7 +42,7 @@ func (ust *UdpServerTrigger) OnInit(args map[string]interface{}, scoped gecko.Ge
 	} else {
 		ust.udpAddr = addr
 	}
-	ust.buffSize = config.GetInt64OrDefault("bufferSizeKB", 1) * 1024
+	ust.bufferSize = config.GetInt64OrDefault("bufferSizeKB", 1) * 1024
 	ust.sendTimeout = config.GetDurationOrDefault("sendTimeout", time.Second*3)
 	ust.recvTimeout = config.GetDurationOrDefault("recvTimeout", time.Second*3)
 	ust.withTag(log.Info).Msg("UDP服务器Trigger初始化")
@@ -74,26 +58,26 @@ func (ust *UdpServerTrigger) OnStart(scoped gecko.GeckoScoped, invoker gecko.Inv
 	go func(shouldBreak <-chan struct{}) {
 		process := func() {
 			// 读取客户端数据
-			buffer := make([]byte, ust.buffSize)
+			buffer := make([]byte, ust.bufferSize)
 			ust.udpConn.SetReadDeadline(time.Now().Add(ust.recvTimeout))
-			if n, addr, err := ust.udpConn.ReadFromUDP(buffer); nil != err {
-				ust.withTag(log.Error).Err(err).Msg("UDP服务器读取数据失败")
+			if n, addr, reErr := ust.udpConn.ReadFromUDP(buffer); nil != reErr {
+				ust.withTag(log.Error).Err(reErr).Msg("UDP服务器读取数据失败")
 			} else {
 				// 使用Invoker调度内部系统处理，完成后返回给客户端
-				if json := ust.decoder(buffer[:n]); nil != json {
+				if json, deErr := ust.decoder(buffer[:n]); nil == deErr {
 					income := gecko.NewTriggerEvent(ust.topic, json)
 					invoker(income, func(data map[string]interface{}) {
 						ust.udpConn.SetWriteDeadline(time.Now().Add(ust.sendTimeout))
-						if bytes := ust.encoder(data); nil != bytes {
+						if bytes, enErr := ust.encoder(data); nil == deErr {
 							if _, err := ust.udpConn.WriteToUDP(bytes, addr); nil != err {
 								ust.withTag(log.Error).Err(err).Msg("服务器返回客户端数据失败：" + addr.String())
 							}
 						} else {
-							ust.withTag(log.Error).Msg("服务器无法序列化的数据")
+							ust.withTag(log.Error).Err(enErr).Msg("服务器无法序列化的数据")
 						}
 					})
 				} else {
-					ust.withTag(log.Error).Msg("服务器接收到无法解析的数据：" + addr.String())
+					ust.withTag(log.Error).Err(deErr).Msg("服务器接收到无法解析的数据：" + addr.String())
 				}
 			}
 		}
--- codec.go
+++ codec.go
@@ -1,5 +1,7 @@
 package gecko
 
+import "github.com/yoojia/go-gecko/x"
+
 //
 // Author: 陈哈哈 chenyongjia@parkingwang.com, yoojiachen@gmail.com
 //
@@ -9,3 +11,17 @@ type Decoder func(bytes []byte) (map[string]interface{}, error)
 
 // 编码器
 type Encoder func(data map[string]interface{}) ([]byte, error)
+
+////
+
+// 将Byte数据解析成JSON对象
+func JSONDefaultDecoder(bytes []byte) (map[string]interface{}, error) {
+	json := make(map[string]interface{})
+	err := x.UnmarshalJSON(bytes, json)
+	return json, err
+}
+
+// 将JSON对象解析成Bytes对象
+func JSONDefaultEncoder(data map[string]interface{}) ([]byte, error) {
+	return x.MarshalJSON(data)
+}
