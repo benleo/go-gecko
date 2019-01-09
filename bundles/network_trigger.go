e534d7c45ee56912114e825eeba0a3bf8deb5e73
yoojia.chen <yoojiachen@gmail.com>
2019-01-09 15:50:25 +0800

    Update evio server impl

--- bundles/network_trigger.go
+++ bundles/network_trigger.go
@@ -39,11 +39,33 @@ func (nst *NetworkServerTrigger) OnInit(args map[string]interface{}, scoped geck
 
 func (nst *NetworkServerTrigger) ONStart(scoped gecko.GeckoScoped, invoker gecko.Invoker) {
 	nst.withTag(log.Info).Msgf("Network服务器启动，绑定地址： %s", nst.addresses)
+	// Events
+	nst.events.Data = func(conn evio.Conn, in []byte) (out []byte, action evio.Action) {
+		// 使用Invoker调度内部系统处理，完成后返回给客户端
+		if json, deErr := nst.decoder(in); nil == deErr {
+			income := gecko.NewTriggerEvent(nst.topic, json)
+			// 处理并等待结果
+			resp := make(chan map[string]interface{})
+			invoker(income, func(data map[string]interface{}) {
+				resp <- data
+			})
+			// Decode and send back client
+			if bytes, enErr := nst.encoder(<-resp); nil == deErr {
+				out = bytes
+			} else {
+				nst.withTag(log.Error).Err(enErr).Msg("服务器无法序列化的数据")
+			}
+		} else {
+			nst.withTag(log.Error).Err(deErr).Msg("服务器接收到无法解析的数据：" + conn.RemoteAddr().String())
+		}
+		return
+	}
 	go func() {
+		// 绑定服务
 		if err := evio.Serve(nst.events, nst.addresses...); nil != err {
 			nst.withTag(log.Error).Err(err).Msg("Network服务器停止")
 		}
-		// TODO
+		// FIXME 如何停止evio服务？
 	}()
 }
 
