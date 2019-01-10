24a31cad12fd962a07c4a85ee337c418b0473620
yoojia.chen <yoojiachen@gmail.com>
2019-01-10 17:43:20 +0800

    Trigger的Invoker，提供同步执行函数

--- trigger.go
+++ trigger.go
@@ -22,8 +22,27 @@ func NewTriggerEvent(topic string, data map[string]interface{}) *TriggerEvent {
 // 处理结果回调接口
 type OnTriggerCompleted func(data map[string]interface{})
 
+////
+
+// Invoker
 // 用来发起请求，并输出结果
 type Invoker func(event *TriggerEvent, callback OnTriggerCompleted)
+
+// 绑定函数来实现同步执行
+func (in Invoker) Execute(event *TriggerEvent) map[string]interface{} {
+	return <-in.Execute0(event)
+}
+
+// 绑定函数来实现同步执行
+func (in Invoker) Execute0(event *TriggerEvent) <-chan map[string]interface{} {
+	resp := make(chan map[string]interface{}, 1)
+	in(event, func(data map[string]interface{}) {
+		resp <- data
+	})
+	return resp
+}
+
+////
 
 // Trigger是一个负责接收前端事件，并调用 {@link ContextInvoker} 方法函数来向系统内部发起触发事件通知；
 // 内部系统处理完成后，将回调完成函数，返回输出
