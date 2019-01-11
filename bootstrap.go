3bd866bec8f557aa4b773b28be192dff17103945
yoojia.chen <yoojiachen@gmail.com>
2019-01-11 17:18:49 +0800

    更新配置及依赖

--- bootstrap.go
+++ bootstrap.go
@@ -1,13 +1,17 @@
 package gecko
 
 import (
+	"github.com/parkingwang/go-conf"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 // Bootstrap提供一个启动入口
 func Bootstrap(prepare func(engine *Engine)) {
-	config := LoadConfig("conf.d")
+	config, err := conf.LoadConfig("conf.d")
+	if nil != err {
+		_bootstrapTag(log.Panic).Err(err).Msg("加载配置文件出错")
+	}
 	if len(config) <= 0 {
 		_bootstrapTag(log.Panic).Msgf("没有任何配置信息")
 	}
