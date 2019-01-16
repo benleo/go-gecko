9179409829ee2f83fcf9492943bb216c4d013aa5
yoojia.chen <yoojiachen@gmail.com>
2019-01-16 09:10:44 +0800

    修正UdpOutputDevice的地址拼接方式

--- abc/udp_output_device.go
+++ abc/udp_output_device.go
@@ -29,43 +29,48 @@ type UdpOutputDevice struct {
 	udpConn       *net.UDPConn
 }
 
-func (ur *UdpOutputDevice) OnInit(args map[string]interface{}, ctx gecko.Context) {
+func (uo *UdpOutputDevice) OnInit(args map[string]interface{}, ctx gecko.Context) {
 	config := conf.WrapImmutableMap(args)
-	ur.maxBufferSize = config.GetInt64OrDefault("bufferSizeKB", 1) * 1024
-	ur.writeTimeout = config.GetDurationOrDefault("writeTimeout", time.Second*10)
+	uo.maxBufferSize = config.GetInt64OrDefault("bufferSizeKB", 1) * 1024
+	uo.writeTimeout = config.GetDurationOrDefault("writeTimeout", time.Second*10)
 }
 
-func (ur *UdpOutputDevice) OnStart(ctx gecko.Context) {
-	address := ur.GetUnionAddress()
-	ur.withTag(log.Info).Msgf("使用UDP客户端模式，远程地址： %s", address)
+func (uo *UdpOutputDevice) OnStart(ctx gecko.Context) {
+	address := uo.GetUnionAddress()
+	uo.withTag(log.Info).Msgf("使用UDP客户端模式，远程地址： %s", address)
 	if addr, err := net.ResolveUDPAddr("udp", address); err != nil {
-		ur.withTag(log.Panic).Err(err).Msgf("无法创建UDP地址： %s", address)
+		uo.withTag(log.Panic).Err(err).Msgf("无法创建UDP地址： %s", address)
 	} else {
 		if conn, err := net.DialUDP("udp", nil, addr); nil != err {
-			ur.withTag(log.Panic).Err(err).Msgf("无法连接UDP服务端： %s", address)
+			uo.withTag(log.Panic).Err(err).Msgf("无法连接UDP服务端： %s", address)
 		} else {
-			ur.udpConn = conn
+			uo.udpConn = conn
 		}
 	}
 }
 
-func (ur *UdpOutputDevice) OnStop(ctx gecko.Context) {
-	if nil != ur.udpConn {
-		ur.udpConn.Close()
+func (uo *UdpOutputDevice) OnStop(ctx gecko.Context) {
+	if nil != uo.udpConn {
+		uo.udpConn.Close()
 	}
 }
 
-func (ur *UdpOutputDevice) Process(frame gecko.PacketFrame, ctx gecko.Context) (gecko.PacketFrame, error) {
-	if err := ur.udpConn.SetWriteDeadline(time.Now().Add(ur.writeTimeout)); nil != err {
+func (uo *UdpOutputDevice) Process(frame gecko.PacketFrame, ctx gecko.Context) (gecko.PacketFrame, error) {
+	if err := uo.udpConn.SetWriteDeadline(time.Now().Add(uo.writeTimeout)); nil != err {
 		return nil, err
 	}
-	if _, err := ur.udpConn.Write(frame); nil != err {
+	if _, err := uo.udpConn.Write(frame); nil != err {
 		return nil, err
 	} else {
 		return gecko.PacketFrame([]byte{}), nil
 	}
 }
 
-func (ur *UdpOutputDevice) withTag(f func() *zerolog.Event) *zerolog.Event {
+// 改写Union地址的拼接方式
+func (uo *UdpOutputDevice) GetUnionAddress() string {
+	return uo.GetGroupAddress() + ":" + uo.GetPrivateAddress()
+}
+
+func (uo *UdpOutputDevice) withTag(f func() *zerolog.Event) *zerolog.Event {
 	return f().Str("tag", "UdpOutputDevice")
 }
