c6d8f3b539d545092b1f222167f04437b144aa93
yoojia.chen <yoojiachen@gmail.com>
2019-03-01 21:44:32 +0800

    添加默认策略Driver实现

--- drivers/strategy_driver.go
+++ drivers/strategy_driver.go
@@ -9,6 +9,12 @@ import (
 // Author: 陈哈哈 chenyongjia@parkingwang.com, yoojiachen@gmail.com
 //
 
+func StrategyDriverFactory() (string, gecko.BundleFactory) {
+	return "StrategyDriver", func() interface{} {
+		return NewStrategyDriver()
+	}
+}
+
 func NewStrategyDriver() *StrategyDriver {
 	return &StrategyDriver{
 		AbcDriver:  gecko.NewAbcDriver(),
@@ -33,7 +39,7 @@ type StrategyDriver struct {
 }
 
 // 添加驱动触发策略
-func (d *StrategyDriver) AddDriveStrategy(strategy DriveStrategy)  {
+func (d *StrategyDriver) AddDriveStrategy(strategy DriveStrategy) {
 	d.strategies = append(d.strategies, strategy)
 }
 
@@ -43,6 +49,48 @@ func (d *StrategyDriver) GetInitArgs() *cfg.Config {
 
 func (d *StrategyDriver) OnInit(args *cfg.Config, ctx gecko.Context) {
 	d.initArgs = args
+
+	strategies := args.MustConfig("strategies")
+	zap := gecko.Zap()
+	defer zap.Sync()
+
+	strategies.ForEach(func(name string, value interface{}) {
+		rule := cfg.Wrap(value.(map[string]interface{}))
+		matchFields, err := rule.GetStringMapOrDefault("matchEventFields", make(map[string]string, 0))
+		if err != nil {
+			zap.Panicf("matchEventFields字段格式错误[TABLE]", err)
+		}
+		targetUUID := rule.MustString("targetUUID")
+		targetCommand := rule.MustConfig("targetCommand")
+
+		if 0 == len(matchFields) || "" == targetUUID || targetCommand.IsEmpty() {
+			zap.Panicf("未正确配置匹配规则： matchFields= %s, targetUUID= %s, targetCommand=%s",
+				matchFields, targetUUID, targetCommand.RefMap())
+		} else {
+			zap.Debugf("联动配置规则： matchFields= %s, targetUUID= %s, targetCommand=%s",
+				matchFields, targetUUID, targetCommand.RefMap())
+		}
+
+		d.AddDriveStrategy(func(event map[string]interface{}) *ConnectedDevice {
+			// 检查是否匹配字段
+			matches := true
+			for k, v := range matchFields {
+				if cfg.Value2String(v) == event[k] {
+					matches = false
+					break
+				}
+			}
+			if !matches {
+				return nil
+			}
+
+			return &ConnectedDevice{
+				DeviceUUID: targetUUID,
+				Payload:    targetCommand.RefMap(),
+			}
+		})
+	})
+
 }
 
 func (d *StrategyDriver) Handle(session gecko.EventSession, deliverer gecko.OutputDeliverer, ctx gecko.Context) error {
