393b57a8ab9a26a277e2b34b56aa64934d9a01b8
yoojia.chen <yoojiachen@gmail.com>
2019-03-13 16:22:40 +0800

    Fixes nop logic device

--- nop/nop_logic.go
+++ nop/nop_logic.go
@@ -30,16 +30,11 @@ func (s *NopLogicDevice) OnInit(config *cfg.Config, ctx gecko.Context) {
 }
 
 // 检查是否符合逻辑设备的数据
-func (s *NopLogicDevice) IsLogicMatch(json gecko.JSONPacket) bool {
+func (s *NopLogicDevice) CheckIfMatch(json gecko.JSONPacket) bool {
 	return true
 }
 
-// 转换输入的数据
-func (s *NopLogicDevice) TransformInput(topic string, json gecko.JSONPacket) (newTopic string, newJson gecko.JSONPacket) {
-	return topic + "#NOP", json
-}
-
 // 转换返回给设备的数据
-func (s *NopLogicDevice) TransformOutput(json gecko.JSONPacket) (newJson gecko.JSONPacket) {
+func (s *NopLogicDevice) Transform(json gecko.JSONPacket) (newJson gecko.JSONPacket) {
 	return json
 }
